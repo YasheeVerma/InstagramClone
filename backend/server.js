5386fbd666a381f649f5e3579a349eae5a835f17
Yashee Verma <vyashee@gmail.com>
2025-12-14 18:31:47 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors({origin: "*",
+    credentials: true}));
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/profile", profileRoutes)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
